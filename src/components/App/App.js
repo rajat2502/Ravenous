71892d184d4f857e229a376b0e934450ac3f7755
abc <rajat2502@users.noreply.github.com>
2019-07-15 16:53:55 +0530

    Ist stage completed

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../../logo.svg';
+import './App.css';
+import BusinessList from '../BusinessList/BusinessList'
+import SearchBar from '../SearchBar/SearchBar'
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="App">
+        <h1>Ravenous</h1>
+        <SearchBar />
+        <BusinessList />
+      </div>
+    )
+  }
+}
+
+export default App;
